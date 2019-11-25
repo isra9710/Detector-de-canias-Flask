f70f5572b088c65c45fb59174a3ba777d30c898b
isra9710 <isra.rios.con@gmail.com>
2019-11-25 02:13:35 -0600

    Merge branch 'master' of https://github.com/isra9710/canias

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for
 import os
+from os import remove
 import cv2
 import uuid
 from scipy.spatial import distance as dist
@@ -112,12 +113,99 @@ def eliminarUsuario(id):
     flash("Usuario eliminado con éxito")
     return redirect((url_for('crudUsuario')))
 
-
+def midpoint(ptA, ptB):
+	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+    
+def agregarImagen(f):
+    folder = os.path.realpath(__file__).replace('\\', '/').split('/')[0:-1]
+    f.save('/'.join(folder) + '/static/' + f.filename)
+    nombre = str(f.filename)
+    return nombre
 
 @app.route("/crudCania", methods=['GET','POST'])
 def crudCania():
     resultados = Resultado.query.all()
     return render_template("administrador/crudCania.html", resultados=resultados)
+
+
+@app.route("/experimento", methods=['GET','POST'])
+def experimento():
+    if request.method == "POST":
+        f = request.files['file']
+        nombre = agregarImagen(f)
+        image = cv2.imread("static/" + nombre)#Se lee la imagen
+        image = imutils.resize(image,width=300, height=500)#Se redimensiona
+        img = cv2.imread("static/" + nombre,1)
+        img = imutils.resize(img,width=300, height=500)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)#
+        gris = gray
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        edged = cv2.Canny(gray, 50, 100)
+        bordes = edged
+        edged = cv2.dilate(edged, None, iterations=1)
+        edged = cv2.erode(edged, None, iterations=1)
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        (cnts, _) = contours.sort_contours(cnts)
+        pixelsPerMetric = None
+        i = 0
+        array = ""
+        dimA = ""
+        dimB = ""
+        for c in cnts:
+            
+            if cv2.contourArea(c) < 100:
+                continue
+            i= i+1
+            orig = image.copy()
+            box = cv2.minAreaRect(c)
+            box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+            box = np.array(box, dtype="int")
+            box = perspective.order_points(box)
+            cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+            for (x, y) in box:
+                cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1) 
+            (tl, tr, br, bl) = box
+            (tltrX, tltrY) = midpoint(tl, tr)
+            (blbrX, blbrY) = midpoint(bl, br)
+            (tlblX, tlblY) = midpoint(tl, bl)
+            (trbrX, trbrY) = midpoint(tr, br)
+            cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+            cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+                (255, 0, 255), 2)
+            cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+                (255, 0, 255), 2)
+            dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+            dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY)) 
+            if pixelsPerMetric is None:
+                pixelsPerMetric = dB / 4.2
+            dimA = dA / pixelsPerMetric
+            dimB = dB / pixelsPerMetric   
+            cv2.putText(orig, str (dimA) + "cm",
+                (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+                0.65, (255, 255, 255), 2)
+            cv2.putText(orig, str (dimB) + "cm",
+                (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+                0.65, (255, 255, 255), 2)
+            if i == 2:
+                if cv2.contourArea(c) >800: # filter small contours
+                    x,y,w,h = cv2.boundingRect(c) # offsets - with this you get 'mask'
+                    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+                    print('Color promdio de la figura (Azul, Verde, Rojo): ',np.array(cv2.mean(img[y:y+h,x:x+w])).astype(np.uint8))
+                    array = np.array(cv2.mean(img[y:y+h,x:x+w])).astype(np.uint8)
+        nombreM = str(
+            uuid.uuid4()) + ".png"  # uuid4 regresa un objeto, no una cadena. Por eso lo convertimos
+        guardar = "static/" + nombreM
+        cv2.imwrite(guardar, orig)
+        nombreC = str(
+            uuid.uuid4()) + ".png"  # uuid4 regresa un objeto, no una cadena. Por eso lo convertimos
+        guardar = "static/" + nombreC
+        cv2.imwrite(guardar, img)
+        flash("Antes de guardar el experimento asegurate de que la imagen fue procesada de manera adecuada")
+        return render_template("administrador/ver.html", nombre = nombre, medida = nombreM, color = nombreC, colores = array, anchura = dimB, altura = dimA)
 
 
 @app.route("/agregarExp", methods=['GET','POST'])
@@ -126,86 +214,11 @@ def agregarExp():
         nombre = session['username']
 
 
-def midpoint(ptA, ptB):
-	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
 
-def procesarImagen():
-    image = cv2.imread("IMG_20191120_021805.jpg")#Se lee la imagen
-    image = imutils.resize(image,width=300, height=500)#Se redimensiona
-    img = cv2.imread("IMG_20191120_021805.jpg",1)
-    img = imutils.resize(img,width=300, height=500)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)#
-    gris = gray
-    gray = cv2.GaussianBlur(gray, (7, 7), 0)
-    edged = cv2.Canny(gray, 50, 100)
-    bordes = edged
-    edged = cv2.dilate(edged, None, iterations=1)
-    edged = cv2.erode(edged, None, iterations=1)
-    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    (cnts, _) = contours.sort_contours(cnts)
-    pixelsPerMetric = None
-    i = 0
-    for c in cnts:
-        
-        if cv2.contourArea(c) < 100:
-            continue
-        i= i+1
-        orig = image.copy()
-        box = cv2.minAreaRect(c)
-        box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-        box = np.array(box, dtype="int")
-        box = perspective.order_points(box)
-        cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
-        for (x, y) in box:
-            cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1) 
-        (tl, tr, br, bl) = box
-        (tltrX, tltrY) = midpoint(tl, tr)
-        (blbrX, blbrY) = midpoint(bl, br)
-        (tlblX, tlblY) = midpoint(tl, bl)
-        (trbrX, trbrY) = midpoint(tr, br)
-        cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
-        cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
-            (255, 0, 255), 2)
-        cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
-            (255, 0, 255), 2)
-        dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-        dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY)) 
-        if pixelsPerMetric is None:
-            pixelsPerMetric = dB / 4.2
-        dimA = dA / pixelsPerMetric
-        dimB = dB / pixelsPerMetric
-        print(dimA)
-        print(dimB)
-        cv2.putText(orig, str (dimA) + "cm",
-            (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
-            0.65, (255, 255, 255), 2)
-        cv2.putText(orig, str (dimB) + "cm",
-            (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
-            0.65, (255, 255, 255), 2)
-        if i == 2:
-            if cv2.contourArea(c) >800: # filter small contours
-                x,y,w,h = cv2.boundingRect(c) # offsets - with this you get 'mask'
-                cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-                cv2.imshow('cutted contour',img[y:y+h,x:x+w])
-                print('Color promdio de la figura (Azul, Verde, Rojo): ',np.array(cv2.mean(img[y:y+h,x:x+w])).astype(np.uint8))
-                array = np.array(cv2.mean(img[y:y+h,x:x+w])).astype(np.uint8)
-                """cv2.waitKey(0)
-                cv2.imshow('img',img) 
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()"""
-        """cv2.imshow("Gris",gris)
-        cv2.imshow("Gauss", gray)
-        cv2.imshow("Bordes", bordes)
-        cv2.imshow("Image", orig)
-        cv2.waitKey()"""
-
-
-
+@app.route("/agregaRep", methods=['GET','POST'])
+def agregaRep():
+    return "hola"
 
 
 if __name__ == '__main__':
